55076eaad53cec7057e2a4aea4010dfeadc570a7
BrianoFelps <briansticknodesbr@gmail.com>
2024-10-17 20:01:37 -0300

    echo e print

---
+++ echo_print.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo '<p>Abóbora</p>';
+        echo print('<p>Abóbora</p>'); // Resultado será o valor 1, pela dupla verificação do print
+    ?>
+</body>
+</html>
