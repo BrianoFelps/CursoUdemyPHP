687e6d218cceecbe10de821fefd8d88e950bfb96
BrianoFelps <briansticknodesbr@gmail.com>
2024-10-20 19:56:38 -0300

    Funções

---
+++ funções.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+            function somarNumeros($num1, $num2){
+                return $num1 + $num2;
+            }
+
+            echo somarNumeros(7, 8);
+
+            // define('multiplicarNumeros', function($num1, $num2){
+            //     echo $num1 * $num2;
+            // });
+
+            // multiplicarNumeros;
+        ?>
+    </body>
+</html>
