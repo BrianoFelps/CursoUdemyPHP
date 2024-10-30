ec1393275b401ad3de6804e0b18299e7776d5cd7
BrianoFelps <briansticknodesbr@gmail.com>
2024-10-30 20:22:32 -0300

    POO: Herança: manutenção e reutilização

--- POO/PilarHerança.php
+++ POO/PilarHerança.php
@@ -1,3 +1,61 @@
 <?php
 
+    class Veiculo{
+        public $placa = null;
+        public $cor = null;
+
+        function frear(){
+            echo 'Freou...';
+        }
+
+        function buzinar(){
+            echo 'bi biiiiiiiii';
+        }
+    }
+
+    class Carro extends Veiculo{
+        private $tetoSolar = null;
+
+        public function __construct($placa, $cor, $tetoSolar) {
+            $this->placa = $placa;
+            $this->cor = $cor;
+            $this->tetoSolar = $tetoSolar;
+        }
+
+        function abrirTetoSolar(){
+            echo 'Abrindo teto solar';
+        }
+    }   
+
+    class Moto extends Veiculo{
+        private $contraPesoGuidao = null;
+
+        public function __construct($placa, $cor, $contraPesoGuidao) {
+            $this->placa = $placa;
+            $this->cor = $cor;
+            $this->contraPesoGuidao = $contraPesoGuidao;
+        }
+        
+        function empinar(){
+            echo 'Empinando';
+        }
+    }
+
+    $carro = new Carro('F4287G8', 'Azul', true);
+    $moto = new Moto('G782JFA', 'Rosa', true);
+
+    echo '<pre>';
+        print_R($carro);
+        echo "<br>";
+        print_R($moto);
+    echo '</pre>';
+
+    echo "</hr>";
+    $carro->buzinar();
+    echo "<br>";
+    $carro->frear();
+    echo "<br>";echo "<br>";
+    $moto->buzinar();
+    echo "<br>";
+    $moto->frear();
 ?>
